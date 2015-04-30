9740805e2697d27c1d0f452c2e2359e6809a29aa
gocha <gochaism@gmail.com>
2015-04-30 22:35:52 +0900

    spc700: Move symbol definitions from loader to processor module

--- src/ldr/snes_spc/snes_spc.cpp
+++ src/ldr/snes_spc/snes_spc.cpp
@@ -2,169 +2,26 @@
 #include "../idaldr.h"
 #include "snes_spc.hpp"
 
-#define BASE_DSP    0x10000
-
 //----------------------------------------------------------------------------
-static sel_t map_dsp()
+static bool map_psram(linput_t *li, uint32 psram_start_in_file)
 {
-  segment_t s;
-  s.startEA = BASE_DSP + 0x00;
-  s.endEA   = BASE_DSP + 0x80;
-  s.type    = SEG_IMEM;
-  s.sel     = allocate_selector(s.startEA >> 4);
-  add_segm_ex(&s, "DSP", NULL, ADDSEG_NOSREG);
-  return s.sel;
-}
+  bool succeeded = false;
 
-//----------------------------------------------------------------------------
-static sel_t map_psram(linput_t *li, uint32 psram_start_in_file)
-{
   segment_t s;
   s.startEA = 0x00000;
   s.endEA   = 0x10000;
-  s.type    = SEG_IMEM;
   s.sel     = allocate_selector(s.startEA >> 4);
 
   if ( !file2base(li, psram_start_in_file, s.startEA, s.endEA, FILEREG_PATCHABLE) )
     loader_failure("Failed mapping 0x%x -> [0x%a, 0x%a)\n", psram_start_in_file, s.startEA, s.endEA);
 
-  add_segm_ex(&s, "RAM", NULL, ADDSEG_NOSREG);
-  return s.sel;
-}
+  succeeded = add_segm(s.sel, s.startEA, s.endEA, "RAM", NULL);
+  if ( succeeded )
+    succeeded = true;
+  else
+    loader_failure("Failed adding RAM segment\n", psram_start_in_file, s.startEA, s.endEA);
 
-//----------------------------------------------------------------------------
-static void set_dsp_names()
-{
-  set_name(BASE_DSP + 0x00, "V0VOLL");
-  set_name(BASE_DSP + 0x01, "V0VOLR");
-  set_name(BASE_DSP + 0x02, "V0PL");
-  set_name(BASE_DSP + 0x03, "V0PH");
-  set_name(BASE_DSP + 0x04, "V0SRCN");
-  set_name(BASE_DSP + 0x05, "V0ADSR1");
-  set_name(BASE_DSP + 0x06, "V0ADSR2");
-  set_name(BASE_DSP + 0x07, "V0GAIN");
-  set_name(BASE_DSP + 0x08, "V0ENVX");
-  set_name(BASE_DSP + 0x09, "V0OUTX");
-
-  set_name(BASE_DSP + 0x10, "V1VOLL");
-  set_name(BASE_DSP + 0x11, "V1VOLR");
-  set_name(BASE_DSP + 0x12, "V1PL");
-  set_name(BASE_DSP + 0x13, "V1PH");
-  set_name(BASE_DSP + 0x14, "V1SRCN");
-  set_name(BASE_DSP + 0x15, "V1ADSR1");
-  set_name(BASE_DSP + 0x16, "V1ADSR2");
-  set_name(BASE_DSP + 0x17, "V1GAIN");
-  set_name(BASE_DSP + 0x18, "V1ENVX");
-  set_name(BASE_DSP + 0x19, "V1OUTX");
-
-  set_name(BASE_DSP + 0x20, "V2VOLL");
-  set_name(BASE_DSP + 0x21, "V2VOLR");
-  set_name(BASE_DSP + 0x22, "V2PL");
-  set_name(BASE_DSP + 0x23, "V2PH");
-  set_name(BASE_DSP + 0x24, "V2SRCN");
-  set_name(BASE_DSP + 0x25, "V2ADSR1");
-  set_name(BASE_DSP + 0x26, "V2ADSR2");
-  set_name(BASE_DSP + 0x27, "V2GAIN");
-  set_name(BASE_DSP + 0x28, "V2ENVX");
-  set_name(BASE_DSP + 0x29, "V2OUTX");
-
-  set_name(BASE_DSP + 0x30, "V3VOLL");
-  set_name(BASE_DSP + 0x31, "V3VOLR");
-  set_name(BASE_DSP + 0x32, "V3PL");
-  set_name(BASE_DSP + 0x33, "V3PH");
-  set_name(BASE_DSP + 0x34, "V3SRCN");
-  set_name(BASE_DSP + 0x35, "V3ADSR1");
-  set_name(BASE_DSP + 0x36, "V3ADSR2");
-  set_name(BASE_DSP + 0x37, "V3GAIN");
-  set_name(BASE_DSP + 0x38, "V3ENVX");
-  set_name(BASE_DSP + 0x39, "V3OUTX");
-
-  set_name(BASE_DSP + 0x40, "V4VOLL");
-  set_name(BASE_DSP + 0x41, "V4VOLR");
-  set_name(BASE_DSP + 0x42, "V4PL");
-  set_name(BASE_DSP + 0x43, "V4PH");
-  set_name(BASE_DSP + 0x44, "V4SRCN");
-  set_name(BASE_DSP + 0x45, "V4ADSR1");
-  set_name(BASE_DSP + 0x46, "V4ADSR2");
-  set_name(BASE_DSP + 0x47, "V4GAIN");
-  set_name(BASE_DSP + 0x48, "V4ENVX");
-  set_name(BASE_DSP + 0x49, "V4OUTX");
-
-  set_name(BASE_DSP + 0x50, "V5VOLL");
-  set_name(BASE_DSP + 0x51, "V5VOLR");
-  set_name(BASE_DSP + 0x52, "V5PL");
-  set_name(BASE_DSP + 0x53, "V5PH");
-  set_name(BASE_DSP + 0x54, "V5SRCN");
-  set_name(BASE_DSP + 0x55, "V5ADSR1");
-  set_name(BASE_DSP + 0x56, "V5ADSR2");
-  set_name(BASE_DSP + 0x57, "V5GAIN");
-  set_name(BASE_DSP + 0x58, "V5ENVX");
-  set_name(BASE_DSP + 0x59, "V5OUTX");
-
-  set_name(BASE_DSP + 0x60, "V6VOLL");
-  set_name(BASE_DSP + 0x61, "V6VOLR");
-  set_name(BASE_DSP + 0x62, "V6PL");
-  set_name(BASE_DSP + 0x63, "V6PH");
-  set_name(BASE_DSP + 0x64, "V6SRCN");
-  set_name(BASE_DSP + 0x65, "V6ADSR1");
-  set_name(BASE_DSP + 0x66, "V6ADSR2");
-  set_name(BASE_DSP + 0x67, "V6GAIN");
-  set_name(BASE_DSP + 0x68, "V6ENVX");
-  set_name(BASE_DSP + 0x69, "V6OUTX");
-
-  set_name(BASE_DSP + 0x70, "V7VOLL");
-  set_name(BASE_DSP + 0x71, "V7VOLR");
-  set_name(BASE_DSP + 0x72, "V7PL");
-  set_name(BASE_DSP + 0x73, "V7PH");
-  set_name(BASE_DSP + 0x74, "V7SRCN");
-  set_name(BASE_DSP + 0x75, "V7ADSR1");
-  set_name(BASE_DSP + 0x76, "V7ADSR2");
-  set_name(BASE_DSP + 0x77, "V7GAIN");
-  set_name(BASE_DSP + 0x78, "V7ENVX");
-  set_name(BASE_DSP + 0x79, "V7OUTX");
-
-  set_name(BASE_DSP + 0x0c, "MVOLL");
-  set_name(BASE_DSP + 0x1c, "MVOLR");
-  set_name(BASE_DSP + 0x2c, "EVOLL");
-  set_name(BASE_DSP + 0x3c, "EVOLR");
-  set_name(BASE_DSP + 0x4c, "KON");
-  set_name(BASE_DSP + 0x5c, "KOF");
-  set_name(BASE_DSP + 0x6c, "FLG");
-  set_name(BASE_DSP + 0x7c, "ENDX");
-  set_name(BASE_DSP + 0x0d, "EFB");
-  set_name(BASE_DSP + 0x2d, "PMON");
-  set_name(BASE_DSP + 0x3d, "NON");
-  set_name(BASE_DSP + 0x4d, "EON");
-  set_name(BASE_DSP + 0x5d, "DIR");
-  set_name(BASE_DSP + 0x6d, "ESA");
-  set_name(BASE_DSP + 0x7d, "EDL");
-  set_name(BASE_DSP + 0x0f, "FIR0");
-  set_name(BASE_DSP + 0x1f, "FIR1");
-  set_name(BASE_DSP + 0x2f, "FIR2");
-  set_name(BASE_DSP + 0x3f, "FIR3");
-  set_name(BASE_DSP + 0x4f, "FIR4");
-  set_name(BASE_DSP + 0x5f, "FIR5");
-  set_name(BASE_DSP + 0x6f, "FIR6");
-  set_name(BASE_DSP + 0x7f, "FIR7");
-}
-
-//----------------------------------------------------------------------------
-static void set_spc700_names()
-{
-  set_name(0xf0, "TEST");
-  set_name(0xf1, "CONTROL");
-  set_name(0xf2, "DSPADDR");
-  set_name(0xf3, "DSPDATA");
-  set_name(0xf4, "CPU0");
-  set_name(0xf5, "CPU1");
-  set_name(0xf6, "CPU2");
-  set_name(0xf7, "CPU3");
-  set_name(0xfa, "T0TARGET");
-  set_name(0xfb, "T1TARGET");
-  set_name(0xfc, "T2TARGET");
-  set_name(0xfd, "T0OUT");
-  set_name(0xfe, "T1OUT");
-  set_name(0xff, "T2OUT");
+  return succeeded;
 }
 
 //----------------------------------------------------------------------------
@@ -202,14 +59,13 @@ void idaapi load_file(linput_t *li, ushort /*neflags*/, const char * /*ffn*/)
   // informations from it; e.g., the size of segments.
   set_processor_type("spc700", SETPROC_ALL|SETPROC_FATAL);
 
-  // DSP
-  map_dsp();
+  // Store information for the cpu module
+  netnode node;
+  node.create("$ spc700");
+  node.hashset("device", "spc700");
 
   // PSRAM
   inf.start_cs = map_psram(li, 0x100);
-
-  set_dsp_names();
-  set_spc700_names();
 }
 
 //----------------------------------------------------------------------------
--- src/module/spc700/reg.cpp
+++ src/module/spc700/reg.cpp
@@ -8,6 +8,11 @@
  *
  */
 
+#include <idp.hpp>
+#include <srarea.hpp>
+#include <diskio.hpp>
+#include <enum.hpp>
+
 #include "spc700.hpp"
 
 //--------------------------------------------------------------------------
@@ -29,6 +34,150 @@ static const char *const RegNames[] =
 };
 
 //----------------------------------------------------------------------
+//       Prepare global variables & defines for ../iocommon.cpp
+//----------------------------------------------------------------------
+static netnode helper;
+char device[MAXSTR];
+static size_t numports = 0;
+static ioport_t *ports = NULL;
+
+//--------------------------------------------------------------------------
+//lint -esym(528,ioresp_ok) is not referenced
+static bool ioresp_ok(void)
+{
+  if ( inf.like_binary() )
+    return true;
+  else
+    return get_segm_by_name("RAM") != NULL;
+}
+#define CHECK_IORESP      ioresp_ok()
+#include "../iocommon.cpp"
+
+//----------------------------------------------------------------------
+static tid_t set_dsp_regs_enum()
+{
+  static const char enum_name[] = "DSPRegisters";
+  enum_t id = get_enum(enum_name);
+  if ( id != BADNODE )
+    return id;
+  id = add_enum(-1, enum_name, hexflag());
+
+  add_enum_member(id, "V0VOLL"  , 0x00);
+  add_enum_member(id, "V0VOLR"  , 0x01);
+  add_enum_member(id, "V0PL"    , 0x02);
+  add_enum_member(id, "V0PH"    , 0x03);
+  add_enum_member(id, "V0SRCN"  , 0x04);
+  add_enum_member(id, "V0ADSR1" , 0x05);
+  add_enum_member(id, "V0ADSR2" , 0x06);
+  add_enum_member(id, "V0GAIN"  , 0x07);
+  add_enum_member(id, "V0ENVX"  , 0x08);
+  add_enum_member(id, "V0OUTX"  , 0x09);
+
+  add_enum_member(id, "V1VOLL"  , 0x10);
+  add_enum_member(id, "V1VOLR"  , 0x11);
+  add_enum_member(id, "V1PL"    , 0x12);
+  add_enum_member(id, "V1PH"    , 0x13);
+  add_enum_member(id, "V1SRCN"  , 0x14);
+  add_enum_member(id, "V1ADSR1" , 0x15);
+  add_enum_member(id, "V1ADSR2" , 0x16);
+  add_enum_member(id, "V1GAIN"  , 0x17);
+  add_enum_member(id, "V1ENVX"  , 0x18);
+  add_enum_member(id, "V1OUTX"  , 0x19);
+
+  add_enum_member(id, "V2VOLL"  , 0x20);
+  add_enum_member(id, "V2VOLR"  , 0x21);
+  add_enum_member(id, "V2PL"    , 0x22);
+  add_enum_member(id, "V2PH"    , 0x23);
+  add_enum_member(id, "V2SRCN"  , 0x24);
+  add_enum_member(id, "V2ADSR1" , 0x25);
+  add_enum_member(id, "V2ADSR2" , 0x26);
+  add_enum_member(id, "V2GAIN"  , 0x27);
+  add_enum_member(id, "V2ENVX"  , 0x28);
+  add_enum_member(id, "V2OUTX"  , 0x29);
+
+  add_enum_member(id, "V3VOLL"  , 0x30);
+  add_enum_member(id, "V3VOLR"  , 0x31);
+  add_enum_member(id, "V3PL"    , 0x32);
+  add_enum_member(id, "V3PH"    , 0x33);
+  add_enum_member(id, "V3SRCN"  , 0x34);
+  add_enum_member(id, "V3ADSR1" , 0x35);
+  add_enum_member(id, "V3ADSR2" , 0x36);
+  add_enum_member(id, "V3GAIN"  , 0x37);
+  add_enum_member(id, "V3ENVX"  , 0x38);
+  add_enum_member(id, "V3OUTX"  , 0x39);
+
+  add_enum_member(id, "V4VOLL"  , 0x40);
+  add_enum_member(id, "V4VOLR"  , 0x41);
+  add_enum_member(id, "V4PL"    , 0x42);
+  add_enum_member(id, "V4PH"    , 0x43);
+  add_enum_member(id, "V4SRCN"  , 0x44);
+  add_enum_member(id, "V4ADSR1" , 0x45);
+  add_enum_member(id, "V4ADSR2" , 0x46);
+  add_enum_member(id, "V4GAIN"  , 0x47);
+  add_enum_member(id, "V4ENVX"  , 0x48);
+  add_enum_member(id, "V4OUTX"  , 0x49);
+
+  add_enum_member(id, "V5VOLL"  , 0x50);
+  add_enum_member(id, "V5VOLR"  , 0x51);
+  add_enum_member(id, "V5PL"    , 0x52);
+  add_enum_member(id, "V5PH"    , 0x53);
+  add_enum_member(id, "V5SRCN"  , 0x54);
+  add_enum_member(id, "V5ADSR1" , 0x55);
+  add_enum_member(id, "V5ADSR2" , 0x56);
+  add_enum_member(id, "V5GAIN"  , 0x57);
+  add_enum_member(id, "V5ENVX"  , 0x58);
+  add_enum_member(id, "V5OUTX"  , 0x59);
+
+  add_enum_member(id, "V6VOLL"  , 0x60);
+  add_enum_member(id, "V6VOLR"  , 0x61);
+  add_enum_member(id, "V6PL"    , 0x62);
+  add_enum_member(id, "V6PH"    , 0x63);
+  add_enum_member(id, "V6SRCN"  , 0x64);
+  add_enum_member(id, "V6ADSR1" , 0x65);
+  add_enum_member(id, "V6ADSR2" , 0x66);
+  add_enum_member(id, "V6GAIN"  , 0x67);
+  add_enum_member(id, "V6ENVX"  , 0x68);
+  add_enum_member(id, "V6OUTX"  , 0x69);
+
+  add_enum_member(id, "V7VOLL"  , 0x70);
+  add_enum_member(id, "V7VOLR"  , 0x71);
+  add_enum_member(id, "V7PL"    , 0x72);
+  add_enum_member(id, "V7PH"    , 0x73);
+  add_enum_member(id, "V7SRCN"  , 0x74);
+  add_enum_member(id, "V7ADSR1" , 0x75);
+  add_enum_member(id, "V7ADSR2" , 0x76);
+  add_enum_member(id, "V7GAIN"  , 0x77);
+  add_enum_member(id, "V7ENVX"  , 0x78);
+  add_enum_member(id, "V7OUTX"  , 0x79);
+
+  add_enum_member(id, "MVOLL"   , 0x0c);
+  add_enum_member(id, "MVOLR"   , 0x1c);
+  add_enum_member(id, "EVOLL"   , 0x2c);
+  add_enum_member(id, "EVOLR"   , 0x3c);
+  add_enum_member(id, "KON"     , 0x4c);
+  add_enum_member(id, "KOF"     , 0x5c);
+  add_enum_member(id, "FLG"     , 0x6c);
+  add_enum_member(id, "ENDX"    , 0x7c);
+  add_enum_member(id, "EFB"     , 0x0d);
+  add_enum_member(id, "PMON"    , 0x2d);
+  add_enum_member(id, "NON"     , 0x3d);
+  add_enum_member(id, "EON"     , 0x4d);
+  add_enum_member(id, "DIR"     , 0x5d);
+  add_enum_member(id, "ESA"     , 0x6d);
+  add_enum_member(id, "EDL"     , 0x7d);
+  add_enum_member(id, "FIR0"    , 0x0f);
+  add_enum_member(id, "FIR1"    , 0x1f);
+  add_enum_member(id, "FIR2"    , 0x2f);
+  add_enum_member(id, "FIR3"    , 0x3f);
+  add_enum_member(id, "FIR4"    , 0x4f);
+  add_enum_member(id, "FIR5"    , 0x5f);
+  add_enum_member(id, "FIR6"    , 0x6f);
+  add_enum_member(id, "FIR7"    , 0x7f);
+
+  return id;
+}
+
+//----------------------------------------------------------------------
 static int idaapi notify(processor_t::idp_notify msgid, ...) // Various messages:
 {
   va_list va;
@@ -42,11 +191,33 @@ static int idaapi notify(processor_t::idp_notify msgid, ...) // Various messages
   int code = invoke_callbacks(HT_IDP, msgid, va);
   if ( code ) return code;
 
+  int retcode = 1;
   switch ( msgid )
   {
+    case processor_t::init:
+      helper.create("$ spc700");
+      break;
+    case processor_t::term:
+      free_ioports(ports, numports);
+      break;
+    case processor_t::oldfile:
     case processor_t::newfile:
-      set_default_segreg_value(NULL, rDs, 0);
-      set_default_segreg_value(NULL, rFp,  0);
+      {
+        char buf[MAXSTR];
+        const char *device_ptr = buf;
+        ssize_t len = helper.hashstr("device", buf, sizeof(buf));
+        if ( len <= 0 )
+          device_ptr = "spc700";
+
+        if ( msgid == processor_t::newfile )
+        {
+          set_device_name(device_ptr, IORESP_ALL);
+          set_dsp_regs_enum();
+
+          set_default_segreg_value(NULL, rDs, 0);
+          set_default_segreg_value(NULL, rFp, 0);
+        }
+      }
       break;
 
     default:
@@ -54,7 +225,7 @@ static int idaapi notify(processor_t::idp_notify msgid, ...) // Various messages
   }
   va_end(va);
 
-  return(1);
+  return retcode;
 }
 
 
